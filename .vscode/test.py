e3387d151d3d918a87a1218a48f3820197111328
Kaluvan <deryugo77program@yandex.ru>
2026-01-04 22:00:13 +0300

    new game

--- .vscode/test.py
+++ .vscode/test.py
@@ -1,2 +1,19 @@
-project = "My first program"
-print(project)
+import random
+
+print("--- ИГРА: УГАДАЙ ЧИСЛО ---")
+secret_number = random.randint(1, 10)
+attempts = 0
+
+print("Я загадал число от 1 до 10. Попробуй угадать!")
+
+while True:
+    guess = int(input("Введи свое число: "))
+    attempts += 1
+    
+    if guess < secret_number:
+        print("Больше!")
+    elif guess > secret_number:
+        print("Меньше!")
+    else:
+        print(f"Поздравляю! Ты угадал за {attempts} попыток.")
+        break
